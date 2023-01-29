70c1ddeea4c6d65c515c1fed0345ab93491d76cc
Daniel Salvador <105977232+DanielSalvadorGibHub@users.noreply.github.com>
2023-01-29 20:18:07 -0300

    Create tete.py

---
+++ python/math/tete.py
@@ -0,0 +1,3 @@
+valor_mensal = float(input("Quanto você ganha por mês?" ))
+horas_trabalhadas = int(input("Quantas horas você trabalhou por mês? ")) 
+print(f"Sabendo disso R${valor_mensal / horas_trabalhadas} por hora")
