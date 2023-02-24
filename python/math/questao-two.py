734e9b9ad237e25906a1106399e4a8bf9913d762
Daniel Salvador <105977232+DanielSalvadorGibHub@users.noreply.github.com>
2023-02-24 20:26:24 -0300

    Create questao-two.py

---
+++ python/math/questao-two.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Digite um número:"))
+numero2 = int(input("Digite outro número:"))
+if numero1 > numero2:
+    print(f"O {numero1} é maior que o {numero2} entre os dois tem uma diferença de {numero1-numero2} casa(s).")
+else:
+    print(f"O {numero2} é maior que {numero1} entre os dois tem uma diferença de {numero1-numero2} casa(s).")
+              
