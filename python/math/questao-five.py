8ee42b617b354c73474dab04e7265f61e1294706
Daniel Salvador <105977232+DanielSalvadorGibHub@users.noreply.github.com>
2023-02-24 19:57:54 -0300

    Update questao-five.py

--- python/math/questao-five.py
+++ python/math/questao-five.py
@@ -1,23 +1,30 @@
+#AQUI ESTÃO PRESENTES AS VARIAVEIS QUE VAMOS USAR PARA DESCOBRIR O RESULTADO DO ALUNO:
 primeira = float(input("Qual foi a primeira nota do aluno? "))
 segunda = float(input("Qual foi a segunda nota do aluno? "))
 terceira = float(input("Qual foi a terceira nota do aluno? "))
-soma = primeira+segunda+terceira
-media = soma/3
+#A SOMA TEM QUE SEGUIR A REGRA DE PEMDAS
+media = (primeira+segunda+terceira)/3
 final = "null"
 resultado = False
+# APARTIR DESSE PONTO COMEÇAMOS UM LOOP PARA QUE O CÓDIGO NÃO SE ATROPELE.
 while resultado == False:
+    #AQUI ESTÁ A CONDICIONAL SE O ALUNO FOI APROVADO!
     if media >= 7:
         final = "Aprovado"
         print(f"a sua média nesse trimestre foi {media} logo você foi {final}!")
         resultado = True
+    #AQUI ESTÁ A CONDICIONAL SE O ALUNO ESTÁ EM RECUPERAÇÃO!
     elif media == 6:
         final = "RECUPERAÇÃO"
         print(f"a sua média nesse trimestre foi {media} logo você está em {final}!")
         resultado = True
+    #AQUI ESTÁ A CONDICIONAL SE O ALUNO REPROVAR
     elif media <= 5:
         final = "REPROVADO"
         print(f"a sua média nesse trimestre foi {media} logo você foi {final}!")
         resultado = True
+# QUANDO A VARIAVEL RESULTADO FOR ALTERADA PARA TRUE O LOOP PARA DE RODAR.
+#NA SEQUENCIA ELE EXIBE ESSA TELA ONDE TEM INFORMAÇÕES MAIS DETALHADA SOBRE A MÉDIA
 print("====== AQUI ESTÃO MAIS DETALHES SOBRE O BOLETIM ========")
 print(f"Primeira nota: {primeira} ")
 print(f"Segunda nota: {segunda} ")
