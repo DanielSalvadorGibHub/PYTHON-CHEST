18d34dbee7d9e6f2d96fdd7d8bcafe353735b784
Daniel Salvador <105977232+DanielSalvadorGibHub@users.noreply.github.com>
2023-02-24 20:54:12 -0300

    Create questao-three.py

---
+++ python/math/questao-three.py
@@ -0,0 +1,6 @@
+# Número escolhido pelo usuario:
+numero1 = int(input("-Digite o número: "))
+if numero1 <= 20 and numero1 >=5:
+    print(f"-O valor escolhido foi {numero1} e ele está entre 5 e 20")
+else:
+    print("-O seu número está fora entre 5 e 20.")
