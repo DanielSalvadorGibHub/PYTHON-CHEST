f3a8ebca16c98d04bc67ee80e9f7043a35c78fb4
Daniel Salvador <105977232+DanielSalvadorGibHub@users.noreply.github.com>
2023-02-24 20:27:53 -0300

    Update questao-five.py

--- python/math/questao-five.py
+++ python/math/questao-five.py
@@ -11,17 +11,17 @@ while resultado == False:
     #AQUI ESTÁ A CONDICIONAL SE O ALUNO FOI APROVADO!
     if media >= 7:
         final = "Aprovado"
-        print(f"a sua média nesse trimestre foi {media} logo você foi {final}!")
+        print(f"A sua média nesse trimestre foi {media:.1f} logo você foi {final}!")
         resultado = True
     #AQUI ESTÁ A CONDICIONAL SE O ALUNO ESTÁ EM RECUPERAÇÃO!
     elif media == 6:
         final = "RECUPERAÇÃO"
-        print(f"a sua média nesse trimestre foi {media} logo você está em {final}!")
+        print(f"a sua média nesse trimestre foi {media:.1f} logo você está em {final}!")
         resultado = True
     #AQUI ESTÁ A CONDICIONAL SE O ALUNO REPROVAR
     elif media <= 5:
         final = "REPROVADO"
-        print(f"a sua média nesse trimestre foi {media} logo você foi {final}!")
+        print(f"a sua média nesse trimestre foi {media:.1f} logo você foi {final}!")
         resultado = True
 # QUANDO A VARIAVEL RESULTADO FOR ALTERADA PARA TRUE O LOOP PARA DE RODAR.
 #NA SEQUENCIA ELE EXIBE ESSA TELA ONDE TEM INFORMAÇÕES MAIS DETALHADA SOBRE A MÉDIA
@@ -29,6 +29,6 @@ print("====== AQUI ESTÃO MAIS DETALHES SOBRE O BOLETIM ========")
 print(f"Primeira nota: {primeira} ")
 print(f"Segunda nota: {segunda} ")
 print(f"Terceira nota: {terceira} ")
-print(f"Média geral do curso: {media} ")
+print(f"Média geral do curso: {media:.1f} ")
 print(f"O seu resultado final foi: {final}  ")
 print("========================================================")
