73fc6e120a4ae4bf64458bf1e7ec7a15cd10f70d
Daniel Salvador <105977232+DanielSalvadorGibHub@users.noreply.github.com>
2023-05-22 12:21:40 -0300

    Update DADOSinfo.py

--- python/analise de dados/DADOSinfo.py
+++ python/analise de dados/DADOSinfo.py
@@ -1,2 +1,11 @@
-#vai mostrar todos os valores, colunas e linhas que vão ser analisadas:
-print(data.info())
+import pyautogui as py
+import time as t
+
+#ABRIR JANELA:
+py.click(1897,206)
+#CLICAR PS:
+py.doubleClick(1334,659)
+py.hotkey("ctrl","c")
+#clica planilha:
+py.click(1239,1033)
+py.hotkey("ctrl","v")
